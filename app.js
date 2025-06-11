6962bd45996cdccf67725a2d661f532dcd5cfed2
Allieniaszxy <alakaalamin40@gmail.com>
2025-06-11 12:43:25 -0700

    created a new file containing my todo list app

---
+++ app.js
@@ -0,0 +1,7 @@
+btn1.addEventListener("click", () => {
+  let input = document.getElementById("text");
+
+  let newInput = document.createElement("li");
+  input.value = document.getElementById("newInput").innerHTML;
+  newInput.appendChild();
+});
